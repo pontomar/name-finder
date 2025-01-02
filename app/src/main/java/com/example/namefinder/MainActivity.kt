382ed54c1ff87743a5e4b412772284eab47aff74
ramaflow <pontonglang@protonmail.ch>
2025-01-02 15:30:48 +0100

    styling and UI improvement

--- app/src/main/java/com/example/namefinder/MainActivity.kt
+++ app/src/main/java/com/example/namefinder/MainActivity.kt
@@ -8,23 +8,43 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialogDefaults.containerColor
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -33,6 +53,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.namefinder.ui.theme.NameFinderTheme
+import com.example.namefinder.ui.theme.ocher
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,16 +61,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NameFinderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = ocher)
+                ) { innerPadding ->
                     val navController = rememberNavController()
                     val model: NameFinderViewModel = viewModel()
 
                     NavHost(navController = navController, startDestination = "StartPage") {
                         composable("StartPage") {
                             StartPage(
-                                modifier = Modifier.padding(innerPadding),
-                                navController = navController,
-                                model = model
+                                modifier = Modifier,
+                                //.padding(innerPadding),
+                                navController = navController, model = model
                             )
                         }
                         composable("NameFinder") {
@@ -81,22 +106,45 @@ fun StartPage(
 
         // When the composable leaves the composition, restore the original orientation
         onDispose {
-            activity?.requestedOrientation = originalOrientation
-                ?: ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+            activity?.requestedOrientation =
+                originalOrientation ?: ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
         }
     }
 
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
-            .background(Color.DarkGray)
+            .padding(12.dp, 20.dp)
+            .background(color = Color(0xFFD7B29D)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+
+
     ) {
-        FavoriteList(model.getSelectNames(), model = model)
-        Button(onClick = { navController.navigate("NameFinder") }) {
-            Text("Discover Baby Names", color = Color.White)
+        Spacer(modifier = Modifier.size(50.dp))
+        Text(
+            "FAVORITE NAMES", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.Black
+        )
+        Spacer(modifier = Modifier.size(50.dp))
+
+        Card(colors = CardDefaults.cardColors(containerColor = Color(0xFFFAEDCD))
+        , modifier = Modifier.padding(30.dp, 10.dp)) {
+            FavoriteList(model.getSelectNames(), model = model)
+           // UserInputRow()
+        }
+        Spacer(modifier = Modifier.size(30.dp))
+        Button(
+            onClick = { navController.navigate("NameFinder") },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFFAEDCD)
+            ),
+            shape = RoundedCornerShape(16.dp),
+
+            ) {
+            Text("Discover Baby Names", color = Color.Black)
 
         }
+
     }
 }
 
@@ -117,16 +165,52 @@ fun FavoriteRow(favorite: BabyName, model: NameFinderViewModel) {
             .padding(8.dp)
     ) {
         Text(
-            text = favorite.name,
-            fontSize = 20.sp,
-            modifier = Modifier.weight(1f)
+            text = favorite.name, fontSize = 20.sp, modifier = Modifier.weight(1f).padding(20.dp, 6.dp)
         )
         IconButton(onClick = { model.removeSelectedName(favorite) }) {
             Icon(
                 Icons.Filled.Delete,
                 contentDescription = "Remove Name from List",
-                tint = Color.White
+                tint = Color.Black
             )
         }
     }
 }
+
+/*
+@Composable
+fun UserInputRow(model: NameFinderViewModel) {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        TextField(
+            value = text,
+            onValueChange = { newText -> text = newText },
+            label = { Text("Enter your idea") },
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 8.dp)
+                .background(color = Color.Transparent),
+            singleLine = true,
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedContainerColor = Color.Transparent// Background color for TextField
+            ),           shape = RoundedCornerShape(16.dp),
+        )
+
+        Button(
+            onClick = { model.addSelectedName(text) },
+            modifier = Modifier.padding(start = 8.dp),
+            colors = ButtonDefaults.buttonColors(ocher),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Text("Add", color = Color.Black)
+        }
+    }
+}
+*/
+
